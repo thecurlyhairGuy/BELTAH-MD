2113734c46b397c813353723550740b5a413646d
Beltah Tech👻 <devbeltah87@gmail.com>
2025-02-05 15:16:50 +0300

    Beltah Tech 254 🇰🇪

--- keizzah/keith.js
+++ keizzah/keith.js
@@ -7,7 +7,7 @@ function keith(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "💧";
+        infoComs.reaction = "👻";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
