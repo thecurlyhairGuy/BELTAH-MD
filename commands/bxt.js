cf37e8a1c9a8995ac28edee4b69791b66bd11a02
Beltah Tech👻 <devbeltah87@gmail.com>
2025-02-05 09:36:30 +0300

    BELTAH TECH 254 🇰🇪

--- commands/bxt.js
+++ commands/bxt.js
@@ -146,8 +146,9 @@ keith({ nomCom: "bxd", aliases: ["liste", "helplist", "commandlist"], categorie:
 ┃🚦╰──────────────
 ╰━━━━━━━━━━━━━━━┈⊷\n
  > ${randomQuote}
+ 
 `;
-    let commandsList = "BELTAH-MD COMMANDS";
+    let commandsList = "";
     const sortedCategories = Object.keys(categorizedCommands).sort();
     let commandIndex = 1;
 
@@ -155,7 +156,7 @@ keith({ nomCom: "bxd", aliases: ["liste", "helplist", "commandlist"], categorie:
         commandsList += `\n*♦️ ${toFancyUppercaseFont(category)} ♦️*`;
         const sortedCommands = categorizedCommands[category].sort();
         for (const command of sortedCommands) {
-            commandsList += `\n${commandIndex++}. ${toFancyLowercaseFont(command)}`;
+            commandsList += `\n🫧${commandIndex++}. ${toFancyLowercaseFont(command)}`;
         }
         commandsList += "";
     }
@@ -165,16 +166,16 @@ keith({ nomCom: "bxd", aliases: ["liste", "helplist", "commandlist"], categorie:
     try {
         const senderName = message.sender || message.from;
         await client.sendMessage(message, {
-            text: responseMessage + commandsList,
-            contextInfo: {
-                mentionedJid: [senderName],
-                externalAdReply: {
-                    title: "𝗕𝗘𝗟𝗧𝗔𝗛-𝗠𝗗 𝗕𝗢𝗧" ,
-                    body: "𝗧𝗮𝗽 𝗵𝗲𝗿𝗲 𝘁𝗼 𝗳𝗼𝗹𝗹𝗼𝘄 𝗼𝘂𝗿 𝗰𝗵𝗮𝗻𝗻𝗲𝗹" ,
-                    thumbnailUrl: "https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg" ,
-                    sourceUrl:'https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F' ,
-                    mediaType: 1,
-                    renderLargerThumbnail: true
+            caption: responseMessage + commandsList,
+        contextInfo: {
+          externalAdReply: {
+            title: "𝗕𝗘𝗟𝗧𝗔𝗛-𝗠𝗗 𝗕𝗢𝗧" ,
+            body: "𝗧𝗮𝗽 𝗵𝗲𝗿𝗲 𝘁𝗼 𝗳𝗼𝗹𝗹𝗼𝘄 𝗼𝘂𝗿 𝗰𝗵𝗮𝗻𝗻𝗲𝗹",
+            mediaType: 1,
+            sourceUrl:"https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F",
+            thumbnailUrl: "https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg" ,
+            renderLargerThumbnail: false,
+            showAdAttribution: true,
                 }
             }
         });
