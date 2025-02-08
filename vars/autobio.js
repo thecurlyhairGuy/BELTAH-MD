b88d511b7859b1d2bdf08de089fbf71265ce06ec
Beltah Tech👻 <devbeltah87@gmail.com>
2025-02-08 17:40:24 +0300

    Beltah Tech 254 🇰🇪

--- vars/autobio.js
+++ vars/autobio.js
@@ -3,7 +3,7 @@ const autobio = (zk, conf) => {
     setInterval(() => {
       const date = new Date();
       zk.updateProfileStatus(
-        `${conf.BOT} is active 24/7\n\n${date.toLocaleString('en-US', { timeZone: 'Africa/Nairobi' })} It's a ${date.toLocaleString('en-US', { weekday: 'long', timeZone: 'Africa/Nairobi' })}.`
+        `👻 ${conf.BOT} 👻 is active 24/7\n\n${date.toLocaleString('en-US', { timeZone: 'Africa/Nairobi' })} It's a ${date.toLocaleString('en-US', { weekday: 'long', timeZone: 'Africa/Nairobi' })}.`
       );
     }, 10 * 1000);
   }
