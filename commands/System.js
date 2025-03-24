b98d4203855b0dcc7658195d82a3b9de243f1d5f
Beltah Tech👻 <devbeltah87@gmail.com>
2025-03-24 10:23:02 +0300

    Update System.js

--- commands/System.js
+++ commands/System.js
@@ -50,7 +50,7 @@ function runtime(seconds) {
   const minutes = Math.floor((seconds % 3600) / 60);
   const secondsLeft = Math.floor(seconds % 60);
 
-  return `*╭───────────────━⊷*\n*║0 ᴅᴀʏs*\n*║${hours} ʜᴏᴜʀs*\n*║${minutes} ᴍɪɴᴜᴛᴇs*\n*║${secondsLeft} sᴇᴄᴏɴᴅs* :\n*╰───────────────━⊷*`;
+  return `*╭───────────────━⊷*\n*║0 ᴅᴀʏs*\n*║${hours} ʜᴏᴜʀs*\n*║${minutes} ᴍɪɴᴜᴛᴇs*\n*║${secondsLeft} sᴇᴄᴏɴᴅs*\n*╰───────────────━⊷*`;
 }
 
 // Function to show loading animation
